d1c33a72f2e7a4dd3ebca58593dbc00ce52ef054
yujingbaobao <adela_yu@yeah.net>
2017-05-11 13:46:50 +0800

    change python

--- src/problem1/solution3/Solution.py
+++ src/problem1/solution3/Solution.py
@@ -1,12 +1,15 @@
-def twoSum( nums, target):
-    map = {}
-    for i in range(0, len(nums)):
-        x=target-nums[i]
-        if(map.has_key(x)):
-            return [map[x],i]
-        else:
-            map[nums[i]]=i
+class Solution(object):
+    def twoSum(self,nums, target):
+        map = {}
+        for i in range(0, len(nums)):
+            x = target - nums[i]
+            if (map.has_key(x)):
+                return [map[x], i]
+            else:
+                map[nums[i]] = i
+
 
 nums = [2, 7, 11, 15]
 target = 9
-print twoSum(nums,target)
+solution=Solution()
+print solution.twoSum(nums, target)
